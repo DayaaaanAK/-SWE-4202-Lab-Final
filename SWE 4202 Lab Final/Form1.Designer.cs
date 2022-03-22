017634f40fb13266b9faee250e82136c73e08271
DayaaaanAK <95142739+DayaaaanAK@users.noreply.github.com>
2022-03-22 23:53:44 +0600

    Add files via upload

---
+++ SWE 4202 Lab Final/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace SWE_4202_Lab_Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Student_ID_Input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Student_Info = new System.Windows.Forms.ListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(668, 422);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(50, 20);
+            this.label12.TabIndex = 38;
+            this.label12.Text = "Final:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(668, 462);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(46, 20);
+            this.label11.TabIndex = 37;
+            this.label11.Text = "Viva:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(668, 498);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(146, 20);
+            this.label10.TabIndex = 36;
+            this.label10.Text = "Total (Out of 300):";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(987, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 31);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Search);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(668, 159);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 20);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "Quiz 1:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(668, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 20);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "Quiz 2:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(668, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 20);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Quiz 3:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(668, 296);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 20);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Quiz 4:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(668, 336);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 20);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Quiz Total (Best 3):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(668, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Attendance:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(668, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Mid:";
+            // 
+            // Student_ID_Input
+            // 
+            this.Student_ID_Input.Location = new System.Drawing.Point(705, 60);
+            this.Student_ID_Input.Name = "Student_ID_Input";
+            this.Student_ID_Input.Size = new System.Drawing.Size(235, 22);
+            this.Student_ID_Input.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(668, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "ID:";
+            // 
+            // Student_Info
+            // 
+            this.Student_Info.FormattingEnabled = true;
+            this.Student_Info.ItemHeight = 16;
+            this.Student_Info.Location = new System.Drawing.Point(48, 60);
+            this.Student_Info.Name = "Student_Info";
+            this.Student_Info.Size = new System.Drawing.Size(588, 356);
+            this.Student_Info.TabIndex = 25;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(668, 581);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 20);
+            this.label9.TabIndex = 49;
+            this.label9.Text = "Grade:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(668, 539);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(99, 20);
+            this.label13.TabIndex = 50;
+            this.label13.Text = "Percentage:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1306, 671);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Student_ID_Input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Student_Info);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Student_ID_Input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox Student_Info;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ SWE 4202 Lab Final/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SWE_4202_Lab_Final
+{
+    public partial class Form1 : Form
+    {
+        List<string> ID = new List<string>();
+        List<string> Student_Name = new List<string>();
+        List<string> Attendance = new List<string>();
+        List<string> Quiz_1 = new List<string>();
+        List<string> Quiz_2 = new List<string>();
+        List<string> Quiz_3 = new List<string>();
+        List<string> Quiz_4 = new List<string>();
+        List<string> Mid = new List<string>();
+        List<string> Final = new List<string>();
+        List<string> Viva = new List<string>();
+        List<int> Total_Marks = new List<int>();
+        int[] total;
+
+        public Form1()
+        {
+            InitializeComponent();
+            using (var reader = new StreamReader(@"D:\SWE4201MarkSheet.csv"))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    ID.Add(values[0]);
+                    Student_Name.Add(values[1]);
+                    Attendance.Add(values[2]);
+                    Quiz_1.Add(values[3]);
+                    Quiz_2.Add(values[4]);
+                    Quiz_3.Add(values[5]);
+                    Quiz_4.Add(values[6]);
+                    Mid.Add(values[7]);
+                    Final.Add(values[8]);
+                    Viva.Add(values[9]);
+                }
+                reader.Close();
+
+                string dummy = ID[0] + "\t" + "\t" + Student_Name[0] + "\t" + "\t" + "\t" + "\t" + "Percentage" + "\t" + "Grade";
+                Student_Info.Items.Add(dummy);
+
+                var id_list = from id in ID
+                              where id.StartsWith("2000")
+                              select id;
+                var name_list = from name in Student_Name
+                                where !name.StartsWith("Name")
+                                select name;
+                var attendance = from atd in Attendance
+                                 where !atd.StartsWith("\"Attendance")
+                                 select atd;
+                var quiz_1 = from quiz in Quiz_1
+                             where !quiz.StartsWith("\"Quiz")
+                             select quiz;
+                var quiz_2 = from quiz in Quiz_2
+                             where !quiz.StartsWith("\"Quiz")
+                             select quiz;
+                var quiz_3 = from quiz in Quiz_3
+                             where !quiz.StartsWith("\"Quiz")
+                             select quiz;
+                var quiz_4 = from quiz in Quiz_4
+                             where !quiz.StartsWith("\"Quiz")
+                             select quiz;
+                var mid_list = from mid in Mid
+                               where !mid.StartsWith("\"MID")
+                               select mid;
+                var final_list = from final in Final
+                                 where !final.StartsWith("\"Final")
+                                 select final;
+                var viva_list = from viva in Viva
+                                where !viva.StartsWith("\"Viva")
+                                select viva;
+
+                /*int i = 0;
+                foreach (var quiz in quiz_1)
+                {
+                    
+                    total[i] = Convert.ToInt32(quiz);
+                    i++;
+                }
+                i = 0;
+                foreach (var quiz in quiz_2)
+                {
+
+                    total[i] += Convert.ToInt32(quiz);
+                    i++;
+                }
+                i = 0;
+                foreach (var quiz in quiz_3)
+                {
+
+                    total[i] += Convert.ToInt32(quiz);
+                    i++;
+                }
+                i = 0;
+                foreach (var mid in mid_list)
+                {
+
+                    total[i] += Convert.ToInt32(mid);
+                    i++;
+                }
+                i = 0;
+                foreach (var final in final_list)
+                {
+
+                    total[i] += Convert.ToInt32(final);
+                    i++;
+                }
+                i = 0;
+                foreach (var viva in viva_list)
+                {
+
+                    total[i] += Convert.ToInt32(viva);
+                    i++;
+                }*/
+
+                for (int j=1;j<ID.Count();j++)
+                {
+                    string dum = ID[j] + "\t" + Student_Name[j]/* + "\t" + total[j]*/;
+                    Student_Info.Items.Add(dum);
+                }
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Search(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(Student_ID_Input.Text);
+
+        }
+    }
+}
